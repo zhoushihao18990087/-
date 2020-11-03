93d7648a8c256dd43ab20a2b83adcea148c22ab8
zhoushihao18990087 <1732796645@qq.com>
2020-11-03 19:39:43 +0800

    newlogin

---
+++ LOGIN/app/src/main/java/com/example/login/SQLReadActivity.java
@@ -0,0 +1,14 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SQLReadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_s_q_l_read);
+    }
+}
